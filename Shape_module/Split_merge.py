6370d31f5b00408eef6788f91e6f34a08ef20311
vTechSavyy <psavio5@vt.edu>
2017-12-20 21:37:31 -0500

    Custom Hough Transform added

---
+++ Shape_module/Split_merge.py
@@ -0,0 +1,24 @@
+import scipy as sp
+
+class Split_Merge: 
+    
+    def __init__(self, data): 
+        # 1. Threshold on distance of furthest point in the dataset: (d): 
+        self.d = 0.1   # Default value: 
+        
+        # 2. Other parameters: 
+        
+        # 3. Input Dataset: 
+        self.dataset =[]
+        self.dataset = data  # Data should be a 2-D Numpy array of X-Y points
+        
+        
+    def split(): 
+        
+        # Find a way to fit a line to the dataset of this class: 
+        
+        # Find a way to get the normal distance of each point from the fitted: 
+        
+        # 
+        
+        
